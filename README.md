eb22be07fcd03bae91a6b53eb24073353fef167d
ctf0 <muah003@gmail.com>
2020-01-15 13:13:32 +0200

    001

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# vscode-auto-surround
+you can have the same behavior with snippet but
+
+- i got tired of having to create a snippet per each character
+- the command `when` failing from realease to another [#86954](https://github.com/microsoft/vscode/issues/86954)
+- some chracters surround text as expected in some languages but not in other
+
+so this extension fixes that for good.
+
+![demo](https://user-images.githubusercontent.com/7388088/72429426-97d38580-3798-11ea-9a47-322a6ab21679.gif)
+
+## Notes
+
+- the extension doesn't need any configuration, just make a selection and press the surrounding key
+- doesn't support input's because its not exposed to api yet
---
+++ extension.js
@@ -0,0 +1,65 @@
+const vscode = require('vscode')
+const PACKAGE_NAME = 'auto-surround'
+
+let config = {}
+let keysList = []
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+async function activate(context) {
+    await readConfig()
+
+    vscode.workspace.onDidChangeConfiguration(async (e) => {
+        if (e.affectsConfiguration(PACKAGE_NAME)) {
+            await readConfig()
+        }
+    })
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand('type', async (e) => {
+            let { text } = e
+            let editor = vscode.window.activeTextEditor
+            let { selections } = editor
+            let newSelections = []
+
+            for (const select of selections) {
+                let { start, end } = select
+                let range = new vscode.Range(start, end)
+
+                if (!range.isEmpty && keysList.includes(text)) {
+                    await editor.edit(
+                        (edit) => {
+                            edit.insert(start, text)
+                            edit.insert(end, text)
+
+                            newSelections.push(new vscode.Selection(
+                                start.line,
+                                start.character,
+                                end.line,
+                                end.character + text.length * 2
+                            ))
+                        },
+                        { undoStopBefore: true, undoStopAfter: false }
+                    )
+                }
+            }
+
+            newSelections.length
+                ? editor.selections = newSelections
+                : vscode.commands.executeCommand('default:type', { text: text })
+        })
+    )
+}
+
+async function readConfig() {
+    config = await vscode.workspace.getConfiguration(PACKAGE_NAME)
+    keysList = config.list
+}
+
+function deactivate() { }
+
+module.exports = {
+    activate,
+    deactivate
+}
