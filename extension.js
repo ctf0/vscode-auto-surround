ac54cbf3a949362661668fb12bc8dae711f1115f
ctf0 <muah003@gmail.com>
2023-03-26 02:04:20 +0200

    012

--- extension.js
+++ extension.js
@@ -1,56 +1,56 @@
-const vscode = require('vscode')
-const PACKAGE_NAME = 'autoSurround'
-const escapeStringRegexp = require('escape-string-regexp')
+const vscode = require('vscode');
+const PACKAGE_NAME = 'autoSurround';
+const escapeStringRegexp = require('escape-string-regexp');
 
-let config = {}
-let keysList = []
-let leftKeysList = []
-let rightKeysList = []
-let escapedKeysList = null
+let config = {};
+let keysList = [];
+let leftKeysList = [];
+let rightKeysList = [];
+let escapedKeysList = null;
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 async function activate(context) {
-    await readConfig()
+    await readConfig();
 
     vscode.workspace.onDidChangeConfiguration(async (e) => {
         if (e.affectsConfiguration(PACKAGE_NAME)) {
-            await readConfig()
+            await readConfig();
         }
-    })
+    });
 
-    context.subscriptions.push(vscode.commands.registerCommand('type', doStuff))
+    context.subscriptions.push(vscode.commands.registerCommand('type', doStuff));
 }
 
 async function doStuff(e) {
-    let {text} = e
-    let editor = vscode.window.activeTextEditor
-    let {document, selections} = editor
-    let newSelections = false
-    let dir = null
+    let {text} = e;
+    let editor = vscode.window.activeTextEditor;
+    let {document, selections} = editor;
+    let newSelections = false;
+    let dir = null;
 
     for (const selection of invertSelections(selections)) {
-        let selectedText = document.getText(selection)
-        let areEqual = allEqual(selectedText)
-        let underLen = selectedText.length > 1 && selectedText.length <= config.ignoreSurroundMappedCharsLength
+        let selectedText = document.getText(selection);
+        let areEqual = allEqual(selectedText);
+        let underLen = selectedText.length > 1 && selectedText.length <= config.ignoreSurroundMappedCharsLength;
 
-        let isLeft = leftKeysList.includes(text)
-        let isRight = rightKeysList.includes(text)
-        let isSupported = keysList.includes(text) || isLeft || isRight
+        let isLeft = leftKeysList.includes(text);
+        let isRight = rightKeysList.includes(text);
+        let isSupported = keysList.includes(text) || isLeft || isRight;
 
         // replace each
         if (config.replaceAllWithMappedChars && underLen && areEqual && isSupported) {
-            newSelections = true
-            let char = selectedText.charAt(0)
-            let replacement = selectedText.replace(new RegExp(escapeStringRegexp(char), 'g'), text)
+            newSelections = true;
+            let char = selectedText.charAt(0);
+            let replacement = selectedText.replace(new RegExp(escapeStringRegexp(char), 'g'), text);
 
             await editor.edit(
                 (edit) => edit.replace(selection, replacement),
-                {undoStopBefore: true, undoStopAfter: true}
-            )
+                {undoStopBefore: false, undoStopAfter: false},
+            );
 
-            continue
+            continue;
         }
 
         // do nothing
@@ -59,50 +59,50 @@ async function doStuff(e) {
             (config.ignoreSurroundIfLast && new RegExp(`(${escapedKeysList})$`, 'i').test(selectedText)) ||
             (config.ignoreSurroundMappedChars && underLen && areEqual)
         ) {
-            continue
+            continue;
         }
 
         // surround
         if (!selection.isEmpty && isSupported) {
-            newSelections = true
-            let {start, end} = selection
+            newSelections = true;
+            let {start, end} = selection;
 
             await editor.edit(
                 (edit) => {
                     if (isLeft) {
-                        dir = 'l'
+                        dir = 'l';
 
-                        edit.insert(start, text)
+                        edit.insert(start, text);
                     } else if (isRight) {
-                        dir = 'r'
+                        dir = 'r';
 
-                        edit.insert(end, text)
+                        edit.insert(end, text);
                     } else {
-                        dir = null
+                        dir = null;
 
-                        edit.insert(start, text)
-                        edit.insert(end, text)
+                        edit.insert(start, text);
+                        edit.insert(end, text);
                     }
                 },
-                {undoStopBefore: true, undoStopAfter: true}
-            )
+                {undoStopBefore: false, undoStopAfter: false},
+            );
         }
     }
 
     newSelections
         ? updateSelection(dir)
-        : vscode.commands.executeCommand('default:type', {text: text})
+        : vscode.commands.executeCommand('default:type', {text: text});
 }
 
 function updateSelection(dir) {
-    let newSelections = []
-    let isOutIn = config.mode == 'out-in'
-    let editor = vscode.window.activeTextEditor
-    let {selections} = editor
+    let newSelections = [];
+    let isOutIn = config.mode == 'out-in';
+    let editor = vscode.window.activeTextEditor;
+    let {selections} = editor;
 
     for (const selection of selections) {
-        let {start, end} = selection
-        let range = null
+        let {start, end} = selection;
+        let range = null;
 
         switch (dir) {
             case 'l':
@@ -110,67 +110,69 @@ function updateSelection(dir) {
                     start.line,
                     isOutIn ? start.character : start.character - 1,
                     end.line,
-                    end.character
-                )
-                break
+                    end.character,
+                );
+                break;
             case 'r':
                 range = new vscode.Selection(
                     start.line,
                     start.character,
                     end.line,
-                    isOutIn ? end.character - 1 : end.character
-                )
-                break
+                    isOutIn ? end.character - 1 : end.character,
+                );
+                break;
 
             default:
                 range = new vscode.Selection(
                     start.line,
                     isOutIn ? start.character : start.character - 1,
                     end.line,
-                    isOutIn ? end.character - 1 : end.character
-                )
-                break
+                    isOutIn ? end.character - 1 : end.character,
+                );
+                break;
         }
 
-        newSelections.push(range)
+        newSelections.push(range);
     }
 
-    editor.selections = newSelections
+    editor.selections = newSelections;
 }
 
 /* Config --------------------------------------------------------------------- */
 async function readConfig() {
-    config = await vscode.workspace.getConfiguration(PACKAGE_NAME)
-    keysList = config.list
-    leftKeysList = config.oneSideSurround.left
-    rightKeysList = config.oneSideSurround.right
+    config = await vscode.workspace.getConfiguration(PACKAGE_NAME);
+    keysList = config.list;
+    leftKeysList = config.oneSideSurround.left;
+    rightKeysList = config.oneSideSurround.right;
 
-    escapedKeysList = [...keysList, ...leftKeysList, ...rightKeysList].map((e) => escapeStringRegexp(e)).join('|')
+    escapedKeysList = [...keysList, ...leftKeysList, ...rightKeysList].map((e) => escapeStringRegexp(e)).join('|');
 }
 
 /* Util --------------------------------------------------------------------- */
 function allEqual(str) {
-    let main = str[0]
-    let arr = str.split('')
-    let test = arr.every((char) => char === main)
+    let main = str[0];
+    let arr = str.split('');
+    let test = arr.every((char) => char === main);
 
     return arr.length && test
         ? keysList.includes(main)
-        : false
+        : false;
 }
 
 function invertSelections(arr) {
     return arr.sort((a, b) => {
         if (a.start.line != b.start.line) { // different lines
-            if (a.start.line > b.start.line) return 1
-            if (b.start.line > a.start.line) return -1
+            if (a.start.line > b.start.line) return 1;
+
+            if (b.start.line > a.start.line) return -1;
         } else { // same line
-            if (a.start.character > b.start.character) return 1
-            if (b.start.character > a.start.character) return -1
+            if (a.start.character > b.start.character) return 1;
+
+            if (b.start.character > a.start.character) return -1;
         }
 
-        return 0
-    }).reverse()
+        return 0;
+    }).reverse();
 }
 
 /* --------------------------------------------------------------------- */
@@ -178,5 +180,5 @@ function deactivate() { }
 
 module.exports = {
     activate,
-    deactivate
-}
+    deactivate,
+};
